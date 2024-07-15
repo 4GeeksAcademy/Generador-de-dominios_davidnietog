b92b9dd726d457e2f7782c06943ebddc71b637a4
David Nieto <xampycod@hotmail.com>
2024-07-15 18:39:13 +0000

    Generador de dominios

--- src/app.js
+++ src/app.js
@@ -6,23 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  function generador() {
-    var palabras = {
-         pronoun: [ "The", "Your", "Our", "Mine", "They"],
-         adj: [ "big", "great", "amazing", "Illegal", "Large", "Obese", "Hot"],
-         noun: [ "Advice", "Website", "Racoon", "Lion", "Band", "Newspapper", "Console", "Call-center", "Assistant", "Bike"],
-    }
-    var ejemplos = []
-        
-        for(p = 0; p < palabras.pronoun.length; p++){
-            for(a = 0; a < palabras.adj.length; a++){
-                for(n = 0; n < palabras.noun.length; n++){
-                   ejemplos.push(palabras.pronoun[p] + palabras.adj[a] + palabras.noun[n] + ".com")
-                 }
-            }
-        }
-        return(ejemplos);
-    }
-    
-    console.log(generador);
   
+  let pronoun = ["the","our"];
+let adj = ["great","big"];
+let noun = ["jogger","racoon"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+
+
+      console.log ( pronoun[i] + adj[j] + noun[k] + ".com");
+
+      
+    }
+  }
+}

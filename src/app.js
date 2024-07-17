f97b8365eabd022a31c12849dab8041176b539a2
David Nieto <xampycod@hotmail.com>
2024-07-17 19:19:11 +0000

    Lineamientos de codigo

--- src/app.js
+++ src/app.js
@@ -5,20 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  
-  let pronoun = ["the","our"];
-let adj = ["great","big"];
-let noun = ["jogger","racoon"];
+window.onload = function() {};
+
+let pronoun = ["the", "our", "his", "we"];
+let adjetive = ["great", "big", "amazing", "small"];
+let noun = ["jogger", "racoon", "dog", "merchant", "drive"];
 
 for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
+  for (let j = 0; j < adjetive.length; j++) {
     for (let k = 0; k < noun.length; k++) {
-
-
-      console.log ( pronoun[i] + adj[j] + noun[k] + ".com");
-
-      
+      pronoun[i] + adjetive[j] + noun[k] + ".com";
     }
   }
 }
